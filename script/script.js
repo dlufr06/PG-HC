15c068b72fe8748168987464a150307427cda033
Daniel Lundberg <danlun@utb.vaxjo.se>
2015-05-06 21:03:33 +0200

    added heading

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){ 
-  
+  $.ajax({
+    method:"GET",
+    url:"http://teknikprogrammet.org/api.demo.php",
+    success:function(data){
+       // console.log(data);
+        $.each(data,function(i, dog){
+            console.log(dog.Namn);
+            });
+    },
+    error:function(){
+        console.log("Error!");
+    }
+    });
     
 });
 
